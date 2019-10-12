b25a3b9a24d47f875cd5697ce9c7e901a9de007c
vss245 <vss245@nyu.edu>
2019-10-12 15:45:51 +0200

    upd

---
+++ projects/visualizer/visualizer.js
@@ -0,0 +1,18 @@
+let song;
+
+function setup() {
+  song = loadSound('ambient.mp3');
+  createCanvas(720, 200);
+  background(255, 0, 0);
+}
+
+function mousePressed() {
+  if (song.isPlaying()) {
+    // .isPlaying() returns a boolean
+    song.stop();
+    background(255, 0, 0);
+  } else {
+    song.play();
+    background(0, 255, 0);
+  }
+}
